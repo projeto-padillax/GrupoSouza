1f34a1d02794af3c67f1f69e040b4e5093b95773
Lucas Dellatorre <lucas.dellatorre@edu.pucrs.br>
2025-07-10 19:10:18 -0300

    add client configuration

--- src/lib/firebase/clientApp.ts
+++ src/lib/firebase/clientApp.ts
@@ -1,7 +1,7 @@
-import firebase from "firebase/app"; 
-import "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
-const clientCredentials = {
+const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
@@ -11,8 +11,8 @@ const clientCredentials = {
     measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 }
 
-if (!firebase.apps.length) {
-    firebase.initializeApp(clientCredentials);
-}
+const app = initializeApp(firebaseConfig);
 
-export default firebase;
+export const db = getFirestore(app);
+
+export default app;
