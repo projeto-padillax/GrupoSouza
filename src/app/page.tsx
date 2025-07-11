173a7db139ad4dbebb05afb15abd282b56b2ec91
Lucas Dellatorre <lucas.dellatorre@edu.pucrs.br>
2025-07-10 22:33:26 -0300

    firestore test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { db } from "@/lib/firebase/clientApp"
+import { getDocs, collection } from "firebase/firestore"
+
+export default async function Home() {
+  const docRef = collection(db, 'teste')
+
+  const collections = await getDocs(docRef)
+
+  const testList: any = collections.docs.map(doc => ({
+    ...doc.data(),
+    id: doc.id
+  }))
+
+  console.log(testList)
+
   return (
     <main >
-     HOME
+      <ul>
+      {testList.map((test: any) => (
+        <li key={test.id}>{test.name}</li>
+      ))}
+      </ul>
     </main>
   );
 }
