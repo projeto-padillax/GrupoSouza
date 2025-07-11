6a03e3a2455b1d827e32acb27f2795b567eb2195
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-07-11 16:31:21 -0300

    add seed user

---
+++ prisma/seed.ts
@@ -0,0 +1,25 @@
+import { PrismaClient, PerfilUsuario } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const danton = await prisma.user.create({
+    data: {
+      name: "Danton",
+      perfil: PerfilUsuario.SUPERADMIN,
+      email: "Danton@prisma.io",
+      telefone: "555191808322",
+      login: "danton",
+      senha: "samurai",
+    },
+  });
+  console.log(danton);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
