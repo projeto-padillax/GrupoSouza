5fe8cdb8a23c529701ad9f9d4ec9f9aa05b3ac7b
João Vitor Schwingel <jv.schwingel@gmail.com>
2025-08-05 16:10:23 -0300

    adicionando mulitplos slides na home

--- src/components/site/slideSection.tsx
+++ src/components/site/slideSection.tsx
@@ -1,35 +1,84 @@
+"use client";
 import Link from "next/link";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import { type CarouselApi } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 
 interface SlideSectionProps {
-  imageUrl?: string;
-  titulo?: string;
-  url?: string;
+  slides: {
+    imagem?: string;
+    titulo?: string;
+    url?: string;
+  }[];
 }
 
-export default function SlideSection(slide: SlideSectionProps) {
+export default function SlideSection({ slides }: SlideSectionProps) {
+  const [api, setApi] = useState<CarouselApi>();
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    if (!api) return;
+
+    const onSelect = () => {
+      const index = api.selectedScrollSnap();
+      setCurrentIndex(index);
+
+      // Impede que avance se estiver no último
+      if (!api.canScrollNext()) {
+        api.scrollTo(index); // trava no último
+      }
+    };
+
+    api.on("select", onSelect);
+    setCurrentIndex(api.selectedScrollSnap());
+
+    return () => {
+      api.off("select", onSelect);
+    };
+  }, [api]);
+
   return (
     <section className="py-8">
       <div className="w-[90%] mx-auto max-w-7xl">
-        <div
-          className="relative h-80 bg-center bg-cover rounded-lg overflow-hidden object-cover"
-          style={{ backgroundImage: `url(${slide.imageUrl})` }}
-        >
-          <Link href={slide.url ?? "#"} className="absolute w-full h-full z-10">
-            {!slide.imageUrl && (
-              <div className="absolute inset-0 bg-black bg-opacity-40"></div>
-            )}
-          </Link>
-          <div
-            className="absolute [background:linear-gradient(0deg,rgba(0,0,0,0.59)_0%,rgba(237,221,83,0)_100%)] bottom-0 left-0 right-0 flex items-end px-[2.4rem]
-  py-[2rem] text-white"
-          >
-            <h2
-              dangerouslySetInnerHTML={{
-                __html: slide.titulo ?? "Slide não encontrado",
-              }}
-              className="font-semibold text-lg text-white text-start max-w-2xl font-sans leading-[1.2]"
-            ></h2>
-          </div>
+        <Carousel setApi={setApi}>
+          <CarouselContent>
+            {slides.map((slide, index) => (
+              <CarouselItem key={index}>
+                <div
+                  className="relative h-80 bg-center bg-cover rounded-3xl overflow-hidden object-cover"
+                  style={{ backgroundImage: `url(${slide.imagem})` }}
+                >
+                  <Link
+                    href={slide.url ?? "#"}
+                    className="absolute w-full h-full z-10"
+                  >
+                    {!slide.imagem && (
+                      <div className="absolute inset-0 bg-black bg-opacity-40"></div>
+                    )}
+                  </Link>
+                  <div className="absolute [background:linear-gradient(0deg,rgba(0,0,0,0.59)_0%,rgba(237,221,83,0)_100%)] bottom-0 left-0 right-0 flex items-end px-[2.4rem] py-[2rem] text-white">
+                    <h2
+                      dangerouslySetInnerHTML={{
+                        __html: slide.titulo ?? "Slide não encontrado",
+                      }}
+                      className="font-semibold text-lg text-white text-start max-w-2xl font-sans leading-[1.2]"
+                    ></h2>
+                  </div>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+        <div className="flex justify-center gap-2 mt-4">
+          {slides.map((_, index) => (
+            <button
+              key={index}
+              onClick={() => api?.scrollTo(index)}
+              className={`w-2 h-2 rounded-full transition-all duration-300 cursor-pointer ${
+                currentIndex === index ? "bg-[#ccc]" : "bg-[#e7e7e7]"
+              }`}
+            />
+          ))}
         </div>
       </div>
     </section>
