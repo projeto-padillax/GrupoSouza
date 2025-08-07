e990c5c4826195194402b6a9735523caedb118ed
Lucas Dellatorre <lucas.dellatorre@edu.pucrs.br>
2025-08-07 19:07:30 -0300

    centralizando destaque

--- src/components/site/destaquesSection.tsx
+++ src/components/site/destaquesSection.tsx
@@ -52,26 +52,28 @@ export function DestaquesSection({ destaques }: DestaquesSectionProps) {
 
   return (
     <section className="w-[90%] mx-auto max-w-7xl">
-      <div className="w-full mt-3 mb-12 flex flex-wrap items-center justify-center md:gap-[35px]">
-        <h2 className="text-[min(max(2rem,4vw),4rem)] font-semibold text-[#333]"> Destaques </h2>
-        <div className="flex justify-center items-center gap-4 pl-[35px] md:border-l border-[#d0d0d0] text-[1.2rem] py-2">
-          {["Alugar", "Comprar", "Lançamentos"].map((tab) => (
-            <button
-              key={tab}
-              onClick={() => setActiveTab(tab)}
-              className={`
-                py-1 border-b-4 inline-block
-                transition-colors duration-300 ease-in-out
-                cursor-pointer
-                ${activeTab === tab ? "border-[#0084d7]" : "border-transparent"}
-                hover:border-[#005a9e]
-              `}
-            >
-              {tab}
-            </button>
-          ))}
-        </div>
-      </div>
+      <div className="w-full mt-3 mb-12 flex flex-col md:flex-row items-center justify-center md:gap-[35px] text-center">
+  <h2 className="text-[min(max(2rem,4vw),4rem)] font-semibold text-[#333] mb-2 md:mb-0">Destaques</h2>
+
+  <div className="flex justify-center items-center gap-4 md:pl-[35px] md:border-l border-[#d0d0d0] text-[1.2rem] py-2">
+    {["Alugar", "Comprar", "Lançamentos"].map((tab) => (
+      <button
+        key={tab}
+        onClick={() => setActiveTab(tab)}
+        className={`
+          py-1 border-b-4 inline-block
+          transition-colors duration-300 ease-in-out
+          cursor-pointer
+          ${activeTab === tab ? "border-[#0084d7]" : "border-transparent"}
+          hover:border-[#005a9e"]
+        `}
+      >
+        {tab}
+      </button>
+    ))}
+  </div>
+</div>
+
 
       <div className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-center gap-5 mb-10">
         {todosImoveis.map((imovel: Destaque) => (
