5de870134dd68b54129ae5047c0cca1168c80b0e
Lucas Dellatorre <lucas.dellatorre@edu.pucrs.br>
2025-08-21 18:32:05 -0300

    add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,7 +2,6 @@ import { getAllSecoes } from '@/lib/actions/secoes'
 import type { MetadataRoute } from 'next'
 import { Imovel } from '@prisma/client'
 import { getAllImoveisForSitemap } from '@/lib/actions/imovel'
-import { slugify } from '@/lib/utils'
 
 function gerarSlug(str: string) {
   return encodeURIComponent(
@@ -19,7 +18,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const secoesRoutes = secoes.map((secao) => ({
     url: secao.url,
     lastModified: new Date(),
-    changeFrequency: 'yearly' as 'yearly',
+    changeFrequency: 'yearly' as const,
     priority: 1,
   }))
 
@@ -30,31 +29,27 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
     // Definir a ação de compra ou aluguel.
     const action = imovel.Finalidade === 'Locação' ? 'alugar' : 'comprar'
 
-const categoriaSlug = imovel.Categoria ? gerarSlug(imovel.Categoria).toLowerCase() : "";
-const cidadeSlug = imovel.Cidade ? gerarSlug(imovel.Cidade) : "";
+    const categoriaSlug = imovel.Categoria ? gerarSlug(imovel.Categoria).toLowerCase() : "";
+    const cidadeSlug = imovel.Cidade ? gerarSlug(imovel.Cidade) : "";
 
-// Função auxiliar para gerar query string XML-safe
-const buildQuery = (params: Record<string, string>) =>
-  new URLSearchParams(params).toString().replace(/&/g, "&amp;");
+    // Função auxiliar para gerar query string XML-safe
+    const buildQuery = (params: Record<string, string>) =>
+      new URLSearchParams(params).toString().replace(/&/g, "&amp;");
 
-// Parâmetros básicos
-const baseParams: Record<string, string> = {
-  action,
-  page: "1",
-};
-  let url;
-// Se houver categoria, adiciona ao query e à URL
-if (categoriaSlug) {
-  baseParams.tipos = categoriaSlug;
-  url = `${process.env.NEXT_PUBLIC_BASE_URL}/busca/${action}/${categoriaSlug}/${cidadeSlug}?${buildQuery(baseParams)}`;
-} else {
-  url = `${process.env.NEXT_PUBLIC_BASE_URL}/busca/${action}/imoveis/${cidadeSlug}?${buildQuery(baseParams)}`;
-}
+    // Parâmetros básicos
+    const baseParams: Record<string, string> = {
+      action,
+      page: "1",
+    };
+
+    const url = categoriaSlug
+      ? `${process.env.NEXT_PUBLIC_BASE_URL}/busca/${action}/${categoriaSlug}/${cidadeSlug}?${buildQuery(baseParams)}`
+      : `${process.env.NEXT_PUBLIC_BASE_URL}/busca/${action}/imoveis/${cidadeSlug}?${buildQuery(baseParams)}`;
 
     return {
       url,
       lastModified: imovel.DataHoraAtualizacao ?? new Date(),
-      changeFrequency: 'weekly' as 'weekly',
+      changeFrequency: 'weekly' as const,
       priority: 0.8,
     }
   })
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,15 +5,4 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export function slugify(text: string): string {
-    return text
-      .normalize("NFD") // separa acentos das letras
-      .replace(/[\u0300-\u036f]/g, "") // remove acentos
-      .replace(/[^a-zA-Z0-9\s-]/g, "") // remove caracteres especiais
-      .trim() // remove espaços extras do começo/fim
-      .replace(/\s+/g, "-") // troca espaços por -
-      .replace(/-+/g, "-") // evita múltiplos hífens
-      .toLowerCase();
-  }
-  
 export const capitalize = (s: string) => s ? s.charAt(0).toUpperCase() + s.slice(1) : s;
